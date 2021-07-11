330e29420becb1ebb78b9ab9d17774466ce75e53
MoshCohenDev <shiko3011@gmail.com>
2021-07-11 15:52:51 +0300

    routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,9 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{path: '', component: () => import('pages/HomePage.vue')},
+    children: [
+      {path: '/', component: () => import('pages/LoginPage.vue')},
+      {path: 'Home', component: () => import('pages/HomePage.vue')},
       {path: 'favorite', component: () => import('pages/FavoritePage.vue')},
       {
         path: '/:catchAll(.*)*',
